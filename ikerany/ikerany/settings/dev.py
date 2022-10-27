29447d98ee6781d3004255ff0bc546327f9fb0fc
Diego Chamba <46286520+dchamba2018@users.noreply.github.com>
2022-10-27 15:22:08 -0500

    Add files via upload

---
+++ ikerany/ikerany/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-%)(uf8g(&*2hzj4e5t938tqf8!wu9*-wiq%!&)%f4cg%^$+6zb"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
